bcee7b75d71791ca8ef6c42f78afa606d7906384
Thales <dan.console.log@gmail.com>
2023-02-26 23:45:37 +0100

    change controllers

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,15 +2,10 @@ import { Request, Response } from "express";
 import UserModel from '../models/User.model'
 
 export const getUsers = (_req: Request, res: Response): void => {
-    UserModel
-        .find()
-        .then(users => res.json(users))
-        .catch(err => console.log(err))
+
 };
 
 export const getOneUser = (req: Request, res: Response): void => {
-    const { id } = req.params
 
-    res.send(`EL AMIGOOOOO CON ID ${id}`)
 }
 
